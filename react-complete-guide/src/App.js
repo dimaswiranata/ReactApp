c623d57db452c6cdc947b96725b360ef6ae25a23
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-04-09 20:53:50 +0700

    setting class names dynamically

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -70,10 +70,19 @@ const App = props => {
     style.backgroundColor='red';
   }
 
+  // let classes = ['red', 'bold'].join(' ');
+  let classes = [];
+  if (personsState.length <= 2){
+    classes.push('red'); // classes = ['red']
+  }
+  if (personsState.length <= 1){
+    classes.push('bold'); // classes = ['red', 'bold']
+  }
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
+      <p className={classes.join(' ')}>This is really working!</p>
       <button 
         onClick={togglePersonsHandler}
         style={style}
